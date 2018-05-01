d2d39b7c7672b808eda3b2a5be92aa73e1e3cf90
knoxaramav2 <wtscott@iastate.edu>
2018-05-01 13:56:33 -0500

    asdasd

---
+++ commit.cmd
@@ -0,0 +1,32 @@
+rem @echo off
+echo Starting to commit
+
+set /P message="Commit message: "
+set /P password="Password: "
+
+echo %message%
+echo %password%
+
+git add .
+
+git commit -m %message%
+
+if errorlevel 1 (
+    echo "Commit Fail"
+    goto :fail
+)
+
+git push
+
+if errorlevel 1 (
+    echo "Push Fail"
+    goto :fail
+)
+
+echo "Git Done"
+pause
+
+
+:fail
+echo "Git Error"
+pause
